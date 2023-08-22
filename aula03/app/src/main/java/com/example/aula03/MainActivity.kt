ec0394ea197b45de493342533e7fac1fdd2d919a
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-08-22 20:23:34 -0300

    fix (gradle build): config ActivityMainBinding

--- aula03/app/src/main/java/com/example/aula03/MainActivity.kt
+++ aula03/app/src/main/java/com/example/aula03/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.aula03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aula03.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var amb: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
