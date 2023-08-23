c9c2e8f391b952312fbee5ca669bdae5c67b0741
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-08-22 22:13:03 -0300

    feat: add logcat comments on cycle

--- aula03/app/src/main/java/com/example/aula03/MainActivity.kt
+++ aula03/app/src/main/java/com/example/aula03/MainActivity.kt
@@ -2,13 +2,49 @@ package com.example.aula03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.aula03.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var amb: ActivityMainBinding
+    private lateinit var amb: ActivityMainBinding
+    private companion object{
+        const val CICLO_PDM = "CICLO_PDM"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         amb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(amb.root)
+
+        Log.v(CICLO_PDM, "onCreate: Iniciando ciclo COMPLETO")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.v(CICLO_PDM, "onStart: Iniciando ciclo VISÍVEL")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.v(CICLO_PDM, "onResume: Iniciando ciclo de PRIMEIRO PLANO")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.v(CICLO_PDM, "onPause: Finalizando ciclo de PRIMEIRO PLANO")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.v(CICLO_PDM, "onStop: Finalizando ciclo VISÍVEL")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.v(CICLO_PDM, "onDestroy: Finalizando ciclo COMPLETO")
     }
 }
